b278a4393aa89f97408f90e602a8f120818f9a9c
Allan Dollé <hello@allandolle.fr>
2022-04-01 20:56:14 +0200

    chore: rollback to nodeLinker node_modules

--- npmpackagejsonlint.config.js
+++ npmpackagejsonlint.config.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+ 
 const { keyOrder } = require('./prettier-package-json.config');
 
 module.exports = {
--- prettier-package-json.config.js
+++ prettier-package-json.config.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+ 
 const { useTabs, tabWidth } = require('./prettier.config');
 
 module.exports = {
