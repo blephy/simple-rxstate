17f4c46439bd7103ee4c37a095f8d6e20063929f
Allan Dollé <hello@allandolle.fr>
2022-05-08 11:18:30 +0200

    chore(release): include all plugins

--- release.config.js
+++ release.config.js
@@ -2,11 +2,52 @@
 module.exports = {
   extends: 'semantic-release-npm-github-publish',
   plugins: [
-    '@semantic-release/git',
-    {
-      assets: ['package.json', 'README.md', 'CHANGELOG.md', 'LICENSE', 'lib/**/*'],
-      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-    },
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          {
+            type: 'build',
+            release: 'patch',
+          },
+          {
+            type: 'ci',
+            release: 'patch',
+          },
+          {
+            type: 'chore',
+            release: 'patch',
+          },
+          {
+            type: 'docs',
+            release: 'patch',
+          },
+          {
+            type: 'refactor',
+            release: 'patch',
+          },
+          {
+            type: 'style',
+            release: 'patch',
+          },
+          {
+            type: 'test',
+            release: 'patch',
+          },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'README.md', 'CHANGELOG.md', 'LICENSE', 'lib/**/*'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
   ],
 };
 /* eslint-enable no-template-curly-in-string -- this is required */
