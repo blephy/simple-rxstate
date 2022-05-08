b3b23938d95f4f70beb58fee36423d34e7488496
Allan Dollé <hello@allandolle.fr>
2022-05-08 11:09:35 +0200

    chore(release): include files in the lib pack

--- release.config.js
+++ release.config.js
@@ -1,3 +1,12 @@
+/* eslint-disable no-template-curly-in-string -- this is required */
 module.exports = {
   extends: 'semantic-release-npm-github-publish',
+  plugins: [
+    '@semantic-release/git',
+    {
+      assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+      message: 'release(version): Release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    },
+  ],
 };
+/* eslint-enable no-template-curly-in-string -- this is required */
