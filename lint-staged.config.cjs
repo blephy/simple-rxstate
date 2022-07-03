624934383bb016c57f4822394c9fcd921fa0b1b0
Allan Dollé <hello@allandolle.fr>
2022-07-03 15:29:29 +0200

    chore(husky): add more extension to lint-staged

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
   'package.json': ['yarn check:packagejson'],
-  '**/*.ts': () => ['tsc -p tsconfig.json --noEmit', 'jest -o'],
-  '**/*.{ts,js}': (filenames) => `eslint ${filenames.join(' ')} --fix --cache`,
+  '**/*.{ts,mts,cts,tsx}': () => ['tsc -p tsconfig.json --noEmit', 'jest -o'],
+  '**/*.{ts,mts,cts,tsx,js,mjs,cjs,jsx}': (filenames) =>
+    `eslint ${filenames.join(' ')} --fix --cache`,
   '*': (filenames) => `prettier --ignore-unknown --write ${filenames.join(' ')}`,
   '**/*.md': (filenames) => `markdownlint-cli2-fix ${filenames.join(' ')}`,
 };
